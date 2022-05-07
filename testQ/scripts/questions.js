1dd7fc0ff06daad85a9cbb35fd515ad55c06cbda
Dare Tunmise <84666598+dare-tunmise@users.noreply.github.com>
2022-05-07 17:05:58 +0100

    Add files via upload

---
+++ testQ/scripts/questions.js
@@ -0,0 +1,215 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+	{
+		numb: 1,
+		question: "What is the capital of Finland?",
+		answer: "Helsinki",
+		options: ["Rabat", "Mushow", "Nileh", "Helsinki"],
+	},
+	{
+		numb: 2,
+		question: "In which decade was Madonna born?",
+		answer: "1950s",
+		options: ["1950s", "1960s", "1970s", "1980s"],
+	},
+	{
+		numb: 3,
+		question: "Who invented the World Wide Web in 1990?",
+		answer: "Tim Berners-Lee",
+		options: ["Elon Musk", "Jeff Bezoz", "Tim Berners-Lee", "Satoshi Nakamoto"],
+	},
+	{
+		numb: 4,
+		question: "Who painted the Mona Lisa?",
+		answer: "Leonardo da Vinci",
+		options: [
+			"Leonardo da Vinci",
+			"Vincent Van Gogh",
+			"Dan Brown",
+			"Leo Tolstoy",
+		],
+	},
+	{
+		numb: 5,
+		question: "What is the largest country in the world?",
+		answer: "Russia",
+		options: ["Nigeria", "China", "India", "Russia"],
+	},
+
+	{
+		numb: 6,
+		question: "What is Scooby Doo's full name?",
+		answer: "Scoobert Doo",
+		options: ["White Doo", "Scoobert Doo", "Johnny Doo", "Ralph Doo"],
+	},
+
+	{
+		numb: 7,
+		question: "What does the AC button on a calculator stand for?",
+		answer: "All Cleared",
+		options: ["All Cleared", "All Cancelled", "All Computed", "All Compounded"],
+	},
+
+	{
+		numb: 8,
+		question: " Who is the current Miss World?",
+		answer: "Karolina Bielawska",
+		options: [
+			"Kiki Håkansson",
+			"Denise Perrier",
+			"Karolina Bielawska",
+			"Toni-Ann Singh",
+		],
+	},
+
+	{
+		numb: 9,
+		question: "To a single decimal point, many kilometers in a mile?",
+		answer: "1.6KM",
+		options: [
+            "2.6KM", 
+            "3.2KM", 
+            "2.4KM", 
+            "1.6KM"
+        ],
+	},
+
+	{
+		numb: 10,
+		question: "What's a baby rabbit called?",
+		answer: "A Kit",
+		options: [
+            "A Kit", 
+            "A kitten", 
+            "A puppy", 
+            "A calf"
+        ],
+	},
+	{
+		numb: 11,
+		question: "Which novel begins, 'It was a bright cold day in April, and the clocks were striking thirteen.'?",
+		answer: "1984",
+		options: [
+            "The Old Man and the Sea", 
+            "Animal Farm", 
+            "1984", 
+            "Lord of the Flies"
+        ],
+	},
+    {
+		numb: 12,
+		question: "What star sign would someone born on August 24th be?",
+		answer: "Virgo",
+		options: [
+            "Aquarius", 
+            "Gemini", 
+            "Virgo", 
+            "Scorpio"
+        ],
+	},
+    {
+		numb: 13,
+		question: "What does He stand for on the periodic table?",
+		answer: "Helium",
+		options: [
+            "Helium", 
+            "Hydrogen", 
+            "Iron", 
+            "Chlorine"
+        ],
+	},
+    {
+		numb: 14,
+		question: "How many minutes in a game of rugby league?",
+		answer: "80 Minutes",
+		options: [
+            "60 Minutes", 
+            "80 Minutes", 
+            "70 Minutes", 
+            "90 Minutes"
+        ],
+	},
+    {
+		numb: 15,
+		question: "What is the smallest country in the world?",
+		answer: "South Africa",
+		options: [
+            "Canada", 
+            "Brazil", 
+            "South Africa", 
+            "Tanzania"
+        ],
+	},
+
+    {
+		numb: 16,
+		question: "What does a Geiger Counter measure?",
+		answer: "Radiation",
+		options: [
+            "Precipitation", 
+            "Insulation", 
+            "Conduction", 
+            "Radiation"
+        ],
+	},
+
+    {
+		numb: 17,
+		question: "Including the recruiter round, how many games were there in Squid Game?",
+		answer: "Eighteen",
+		options: [
+            "Sixteen", 
+            "Twenty", 
+            "Six", 
+            "Eighteen"
+        ],
+	},
+
+    {
+		numb: 18,
+		question: "What grain is the Japanese spirit Sake made from?",
+		answer: "Rice",
+		options: [
+            "Rice", 
+            "Wheat", 
+            "Soya", 
+            "Sunflower"
+        ],
+	},
+
+    {
+		numb: 19,
+		question: "How many sides does a heptadecagon have?",
+		answer: "Seventeen",
+		options: [
+            "Eighteen", 
+            "Seventeen", 
+            "Twelve", 
+            "Nineteen"
+        ],
+	},
+    {
+		numb: 20,
+		question: "Which supermodel was born in Streatham, London?",
+		answer: "Naomi Campbell",
+		options: [
+            "Tyra Banks", 
+            "Karlie Kloss", 
+            "Naomi Campbell", 
+            "Miranda Kerr"],
+	},
+	// you can uncomment the below codes and make duplicate as more as you want to add question
+	// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+	//   {
+	//   numb: 6,
+	//   question: "Your Question is Here",
+	//   answer: "Correct answer of the question is here",
+	//   options: [
+	//     "Option 1",
+	//     "option 2",
+	//     "option 3",
+	//     "option 4"
+	//   ]
+	// },
+];
